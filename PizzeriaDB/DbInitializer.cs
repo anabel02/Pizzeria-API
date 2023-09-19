13a7a34b31d2031e4f15a798de2ac92b291417f8
Anabel Benítez <anabelbenitezglez@gmail.com>
2023-09-18 20:38:16 -0400

    Changing database directory

--- PizzeriaDB/DbInitializer.cs
+++ PizzeriaDB/DbInitializer.cs
@@ -11,40 +11,33 @@ public static class DbInitializer
         {
             return; // DB has been seeded
         }
-
-        if (context.Pizzas is null || !context.Pizzas.Any())
+        
+        var ingredients = new Ingredient[]
         {
-            var pizzas = new Pizza[]
-            {
-                new() { Name = "Margarita", Price = 2.50 },
-                new() { Name = "Hawaiian", Price = 4.75, Size = Size.Large},
-                new() { Name = "Pepperoni", Price = 3.50, Size = Size.Medium},
-                new() { Name = "Bacon", Price = 3.50, Size = Size.Medium},
-                new() { Name = "Vegetarian", Price = 3.50, Size = Size.Medium},
-                new() { Name = "Meat", Price = 3.50, Size = Size.Medium}
-            };
-
-            context.Pizzas?.AddRange(pizzas);
-        }
-
-        if (context.Ingredients is null || !context.Ingredients.Any())
+            new() { Name = "Cheese" },
+            new() { Name = "Tomato sauce" },
+            new() { Name = "Flour" },
+            new() { Name = "Ham" },
+            new() { Name = "Pineapple" },
+            new() { Name = "Mushrooms" },
+            new() { Name = "Pepperoni" },
+            new() { Name = "Bacon" },
+            new() { Name = "Onion" },
+            new() { Name = "Olives" }
+        };
+            
+        var pizzas = new Pizza[]
         {
-            var ingredients = new Ingredient[]
-            {
-                new() { Name = "Cheese" },
-                new() { Name = "Tomato sauce" },
-                new() { Name = "Flour" },
-                new() { Name = "Ham" },
-                new() { Name = "Pineapple" },
-                new() { Name = "Mushrooms" },
-                new() { Name = "Pepperoni" },
-                new() { Name = "Bacon" },
-                new() { Name = "Onion" },
-                new() { Name = "Olives" }
-            };
+            new() { Name = "Margarita", Price = 2.50, Ingredients = new PizzaIngredient[] {new(){ Ingredient = ingredients[0], Units = 4}}},
+            new() { Name = "Hawaiian", Price = 4.75, Size = Size.Large },
+            new() { Name = "Pepperoni", Price = 3.50, Size = Size.Medium },
+            new() { Name = "Bacon", Price = 3.50, Size = Size.Medium },
+            new() { Name = "Vegetarian", Price = 3.50, Size = Size.Medium },
+            new() { Name = "Meat", Price = 3.50, Size = Size.Medium }
+        };
 
-            context.Ingredients?.AddRange(ingredients);
-        }
+        context.Ingredients?.AddRange(ingredients);
+        context.Pizzas?.AddRange(pizzas);
         
         context.SaveChanges();
     }
